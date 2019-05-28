8138c8f5f279240a1a1a66554e1b76e87b0c0fae
Daesik Kim <daesik82@gmail.com>
2019-05-28 12:49:44 +0900

    .sh files added

---
+++ twitter/us_sen_hrep/get_sen_tws.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pid=`ps -ef | grep 'get_sen_tws.py' | grep -v 'grep' | awk '{print $2}'`
+kill -9 $pid
+/home/humpbackwhale/anaconda3/bin/python3 /home/humpbackwhale/data_collection/twitter/us_politics/sen_n_hrep/get_tw_ussen.py &
